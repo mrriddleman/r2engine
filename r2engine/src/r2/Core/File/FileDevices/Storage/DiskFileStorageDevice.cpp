c82e8a3e7136d918f7404de8d4f44174225537f5
mrriddleman <screamstudiosgames@gmail.com>
2019-04-16 23:32:22 -0700

    r2engine: adding file storage devices + sha256

---
+++ r2engine/src/r2/Core/File/FileDevices/Storage/DiskFileStorageDevice.cpp
@@ -0,0 +1,93 @@
+//
+//  DiskFileStorageDevice.cpp
+//  r2engine
+//
+//  Created by Serge Lansiquot on 2019-04-09.
+//
+
+#include "r2/Core/File/FileDevices/Storage/DiskFileStorageDevice.h"
+#include "r2/Core/File/DiskFile.h"
+
+
+namespace r2::fs
+{
+    DiskFileStorageDevice::DiskFileStorageDevice()
+        : FileStorageDevice(DeviceStorage::Disk)
+        , moptrDiskFilePool(nullptr)
+    {
+        
+    }
+    
+    DiskFileStorageDevice::~DiskFileStorageDevice()
+    {
+        R2_CHECK(moptrDiskFilePool == nullptr, "We haven't been unmounted yet!");
+    }
+    
+    bool DiskFileStorageDevice::Mount(r2::mem::LinearArena& permanentStorage, u64 numFilesActive)
+    {
+        if (moptrDiskFilePool)
+        {
+            return true;// we've already mounted
+        }
+        
+        moptrDiskFilePool = MAKE_POOLA(permanentStorage, sizeof(DiskFile), numFilesActive);
+        R2_CHECK(moptrDiskFilePool != nullptr, "We couldn't allocate a pool of size: %llu\n", sizeof(DiskFile)*numFilesActive);
+        
+        return moptrDiskFilePool != nullptr;
+    }
+    
+    bool DiskFileStorageDevice::Unmount(r2::mem::LinearArena& permanentStorage)
+    {
+        if (moptrDiskFilePool == nullptr)
+        {
+            return true;
+        }
+        
+        FREE(moptrDiskFilePool, permanentStorage);
+        moptrDiskFilePool = nullptr;
+        
+        return true;
+    }
+    
+    File* DiskFileStorageDevice::Open(const char* path, FileMode mode)
+    {
+        R2_CHECK(moptrDiskFilePool != nullptr, "You never mounted DiskFileStorageDevice!");
+        if (moptrDiskFilePool == nullptr)
+        {
+            R2_LOGW("Cannot open a file if we haven't been mounted yet!\n");
+            return nullptr;
+        }
+        
+        DiskFile* diskFile = (DiskFile*)moptrDiskFilePool->Allocate(sizeof(DiskFile), alignof(DiskFile), 0);
+        R2_CHECK(diskFile != nullptr, "We couldn't allocate a disk file!");
+        if (diskFile != nullptr && diskFile->Open(path, mode))
+        {
+            diskFile->SetFileDevice(this);
+            
+            return diskFile;
+        }
+        
+        return nullptr;
+    }
+    
+    void DiskFileStorageDevice::Close(File* file)
+    {
+        R2_CHECK(moptrDiskFilePool != nullptr, "You never mounted DiskFileStorageDevice!");
+        
+        if (moptrDiskFilePool == nullptr)
+        {
+            R2_LOGW("Cannot close a file if we haven't been mounted yet!\n");
+            return;
+        }
+        
+        R2_CHECK(file != nullptr, "File passed in is nullptr!");
+        
+        if (file)
+        {
+            DiskFile* diskFile = static_cast<DiskFile*>(file);
+            diskFile->Close();
+            
+            moptrDiskFilePool->Free(file);
+        }
+    }
+}
---
+++ r2engine/src/r2/Core/File/FileDevices/Storage/DiskFileStorageDevice.h
@@ -0,0 +1,33 @@
+//
+//  DiskFileStorageDevice.h
+//  r2engine
+//
+//  Created by Serge Lansiquot on 2019-04-09.
+//
+
+#ifndef DiskFileStorageDevice_h
+#define DiskFileStorageDevice_h
+
+#include "r2/Core/File/FileDevice.h"
+#include "r2/Core/Memory/Allocators/LinearAllocator.h"
+#include "r2/Core/Memory/Allocators/PoolAllocator.h"
+
+namespace r2::fs
+{
+    class R2_API DiskFileStorageDevice: public FileStorageDevice
+    {
+    public:
+        //Maybe pass in a pool of DiskFiles or make them using an allocator or something?
+        DiskFileStorageDevice();
+        ~DiskFileStorageDevice();
+        bool Mount(r2::mem::LinearArena& permanentStorage, u64 numFiles);
+        bool Unmount(r2::mem::LinearArena& permanentStorage);
+        virtual File* Open(const char* path, FileMode mode) override;
+        virtual void Close(File* file) override;
+        
+    private:
+        r2::mem::PoolAllocator* moptrDiskFilePool;
+    };
+}
+
+#endif /* DiskFileStorageDevice_h */
