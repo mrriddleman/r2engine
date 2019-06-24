7c739f12010da22d226e0d709cb91dd50b0247a3
mrriddleman <screamstudiosgames@gmail.com>
2019-06-23 19:00:31 -0700

    r2engine: added AssetLoader header

---
+++ r2engine/src/r2/Core/Assets/AssetLoader.h
@@ -0,0 +1,25 @@
+//
+//  AssetLoader.h
+//  r2engine
+//
+//  Created by Serge Lansiquot on 2019-06-23.
+//
+
+#ifndef AssetLoader_h
+#define AssetLoader_h
+
+namespace r2::asset
+{
+    class AssetBuffer;
+    
+    class AssetLoader
+    {
+    public:
+        
+        virtual bool ShouldProcess() = 0;
+        virtual u64 GetLoadedAssetSize(char* rawBuffer, u64 size) = 0;
+        virtual bool LoadAsset(char* rawBuffer, u64 rawSize, AssetBuffer& assetBuffer) = 0;
+    };
+}
+
+#endif /* AssetLoader_h */
