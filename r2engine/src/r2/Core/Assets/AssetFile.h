9575af45e8b17bab6ce132943742854f30bdbfa7
mrriddleman <screamstudiosgames@gmail.com>
2019-06-23 18:48:39 -0700

    r2engine: addet file

---
+++ r2engine/src/r2/Core/Assets/AssetFile.h
@@ -0,0 +1,28 @@
+//
+//  AssetFile.h
+//  r2engine
+//
+//  Created by Serge Lansiquot on 2019-06-23.
+//
+
+#ifndef AssetFile_h
+#define AssetFile_h
+
+namespace r2::asset
+{
+    class Asset;
+    
+    class AssetFile
+    {
+    public:
+        virtual bool Open() = 0;
+        virtual bool Close() = 0;
+        virtual u64 RawAssetSize(const Asset& asset) = 0;
+        virtual u64 RawAsset(const Asset& asset, char* data) = 0;
+        virtual u64 NumAssets() const = 0;
+        virtual char* GetAssetName(u64 index) const = 0;
+        virtual ~AssetFile() {}
+    };
+}
+
+#endif /* AssetFile_h */
