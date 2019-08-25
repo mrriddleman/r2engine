20fd9ad279f6c25a66d48950bd515ccda9306e8d
mrriddleman <screamstudiosgames@gmail.com>
2019-08-25 15:24:16 -0700

    r2engine: updating git ignore, added asset compiler (not implemented)

---
+++ r2engine/src/r2/Core/Assets/Pipeline/AssetCompiler.cpp
@@ -0,0 +1,8 @@
+//
+//  AssetCompiler.cpp
+//  r2engine
+//
+//  Created by Serge Lansiquot on 2019-08-25.
+//
+
+#include "AssetCompiler.h"
---
+++ r2engine/src/r2/Core/Assets/Pipeline/AssetCompiler.h
@@ -0,0 +1,16 @@
+//
+//  AssetCompiler.h
+//  r2engine
+//
+//  Created by Serge Lansiquot on 2019-08-25.
+//
+
+#ifndef AssetCompiler_h
+#define AssetCompiler_h
+
+namespace r2::asset::pln
+{
+    
+}
+
+#endif /* AssetCompiler_h */
