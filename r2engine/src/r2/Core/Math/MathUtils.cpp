eca27a3fbcb4a488a53ce326ed3a221d6e27338f
mrriddleman <screamstudiosgames@gmail.com>
2019-12-18 17:15:47 -0800

    r2engine: ?

--- r2engine/src/r2/Core/Math/MathUtils.cpp
+++ r2engine/src/r2/Core/Math/MathUtils.cpp
@@ -5,4 +5,21 @@
 //  Created by Serge Lansiquot on 2019-12-16.
 //
 
-#include "MathUtils.hpp"
+#include "MathUtils.h"
+
+namespace r2::math
+{
+    glm::vec3 Lerp(const glm::vec3& v1, const glm::vec3& v2, float t)
+    {
+        return glm::vec3(
+            (1.0f - t) * v1.x + t * v2.x,
+            (1.0f - t) * v1.y + t * v2.y,
+            (1.0f - t) * v1.z + t * v2.z
+        );
+    }
+    
+    glm::quat Slerp(const glm::quat& q1, const glm::quat& q2, float t)
+    {
+        return glm::normalize(glm::mix(q1, q2, t));
+    }
+}
--- r2engine/src/r2/Core/Math/MathUtils.h
+++ r2engine/src/r2/Core/Math/MathUtils.h
@@ -1,13 +1,19 @@
 //
-//  MathUtils.hpp
+//  MathUtils.h
 //  r2engine
 //
 //  Created by Serge Lansiquot on 2019-12-16.
 //
 
-#ifndef MathUtils_hpp
-#define MathUtils_hpp
+#ifndef MathUtils_h
+#define MathUtils_h
 
-#include <stdio.h>
+#include "glm/glm.hpp"
+#include "glm/gtc/quaternion.hpp"
+namespace r2::math
+{
+    glm::vec3 Lerp(const glm::vec3& v1, const glm::vec3& v2, float t);
+    glm::quat Slerp(const glm::quat& q1, const glm::quat& q2, float t);
+}
 
-#endif /* MathUtils_hpp */
+#endif /* MathUtils_h */
