29a07b6af51b66314a42554d0a3cd7eb0dd07238
mrriddleman <screamstudiosgames@gmail.com>
2020-03-08 01:25:48 -0800

    r2engine: figured out issue with lights

--- Sandbox/assets/shaders/raw/PBR.fs
+++ Sandbox/assets/shaders/raw/PBR.fs
@@ -106,16 +106,16 @@ vec3 FresnelSchlickRoughness(float cosTheta, vec3 Fo, float roughness)
 
 float DistributionGGX(vec3 N, vec3 H, float roughness)
 {
-    float a = roughness*roughness;
-    float a2 = a*a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
+	float a = roughness * roughness;
+	float a2 = a * a;
+	float NdotH = max(dot(N, H), 0.0);
+	float NdotH2 = NdotH * NdotH;
 
-    float nom   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
+	float num = a2;
+	float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+	denom = PI * denom * denom;
 
-    return nom / denom;
+	return num / denom;
 }
 
 float GeometrySchlickGGX(float NdotV, float roughness)
