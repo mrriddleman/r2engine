66b5943f17bbdb393dd0ec75a6528506196f108f
serge <screamstudiosgames@gmail.com>
2023-03-07 15:36:17 -0800

    r2engine: added editor name component

---
+++ r2engine/src/r2/Game/ECS/Components/EditorNameComponent.h
@@ -0,0 +1,17 @@
+#ifndef __EDITOR_NAME_COMPONENT_H__
+#define __EDITOR_NAME_COMPONENT_H__
+
+#if defined R2_EDITOR
+
+#include <string>
+
+namespace r2::ecs
+{
+	struct EditorName
+	{
+		std::string editorName;
+	};
+}
+
+#endif
+#endif // __EDITOR_NAME_COMPONENT_H__
