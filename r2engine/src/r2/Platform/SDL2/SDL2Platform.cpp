2afc8a1d0a0c7d66ebbdc8c93dec4296650de4c4
serge <screamstudiosgames@gmail.com>
2023-10-29 23:57:24 -0700

    r2engine: lowered memory required for the engine

--- r2engine/src/r2/Platform/SDL2/SDL2Platform.cpp
+++ r2engine/src/r2/Platform/SDL2/SDL2Platform.cpp
@@ -49,7 +49,7 @@ namespace r2
     const u64 SDL2Platform::MAX_NUM_MEMORY_AREAS = 16;
     
     //@NOTE: Increase as needed for dev
-    const u64 SDL2Platform::TOTAL_INTERNAL_ENGINE_MEMORY = Megabytes(408);
+    const u64 SDL2Platform::TOTAL_INTERNAL_ENGINE_MEMORY = Megabytes(338);
     
     //@NOTE: Should never exceed the above memory
     const u64 SDL2Platform::TOTAL_INTERNAL_PERMANENT_MEMORY = Megabytes(8);
