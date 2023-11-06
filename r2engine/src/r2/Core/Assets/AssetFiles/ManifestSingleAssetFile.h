bd7d237ebdd0099d1c9a26272ed17cd0a691671c
serge <screamstudiosgames@gmail.com>
2023-11-05 23:03:46 -0800

    r2engine: removed some old code

--- r2engine/src/r2/Core/Assets/AssetFiles/ManifestSingleAssetFile.h
+++ r2engine/src/r2/Core/Assets/AssetFiles/ManifestSingleAssetFile.h
@@ -33,31 +33,14 @@ namespace r2::asset
 #ifdef R2_ASSET_PIPELINE
 		virtual bool ReloadFilePath(const std::vector<std::string>& paths, r2::asset::HotReloadType type) override;
 
-
 		virtual bool SaveManifest() const override;
 		virtual void Reload(AssetCache* assetCache) override;
-
 #endif
-	//	virtual bool NeedsManifestData() const override;
-	//	virtual void SetManifestData(const byte* manifestData) override;
 
-		//virtual bool Open(bool writable = false) override;
-		//virtual bool Open(r2::fs::FileMode mode) override;
-		//virtual bool Close() override;
-		//virtual bool IsOpen() const override;
-		//virtual u64 RawAssetSize(const Asset& asset) override;
-		//virtual u64 LoadRawAsset(const Asset& asset, byte* data, u32 dataBufSize) override;
-		//virtual u64 WriteRawAsset(const Asset& asset, const byte* data, u32 dataBufferSize, u32 offset) override;
-		//virtual u64 NumAssets() override;
-		//virtual void GetAssetName(u64 index, char* name, u32 nameBuferSize) override;
-		//virtual u64 GetAssetHandle(u64 index) override;
 		virtual const char* FilePath() const override;
 
 
 	private:
-		//char mPath[r2::fs::FILE_PATH_LENGTH];
-		//r2::fs::File* mFile;
-		//r2::asset::Asset mAsset;
 		
 	};
 }
